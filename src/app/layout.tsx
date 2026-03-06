f0a734ae591ff9cc73c4fa68a9e316791c30a23a
devLee98 <iiyoungchan@naver.com>
2026-03-07 03:10:20 +0900

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -6,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={cn("font-sans", inter.variable)}>
       <body>{children}</body>
     </html>
   );

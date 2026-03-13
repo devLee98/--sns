af72b9637e75b0bc712973d4b3f99be6958b47fc
devLee98 <iiyoungchan@naver.com>
2026-03-13 23:13:39 +0900

    fix:포스트모달 스타일링

--- src/components/modal/posteditmodal.tsx
+++ src/components/modal/posteditmodal.tsx
@@ -4,34 +4,52 @@ import { usePostEditModal } from "@/app/store/posteditmodal";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
 import { ImageIcon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 
 export default function PostEditModal() {
   const { isOpen, close } = usePostEditModal();
 
-  const handleClostModal = () => {
+  const [content, setContent] = useState("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+  const handleCloseModal = () => {
+    setContent("");
     close();
   };
 
-  // 1. 서버(SSR)에서는 바로 null 반환 (document 없음)
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [content]);
+
+  // 서버에서는 바로 null 반환 (document 없음)
   if (typeof window === "undefined") {
     return null;
   }
 
-  // 2. 클라이언트에서만 modal-root를 찾고, 없으면 null
+  // 클라이언트에서만 modal-root를 찾고, 없으면 null
   const modalRoot = document.getElementById("modal-root");
   if (!modalRoot) return null;
 
   return createPortal(
-    <Dialog open={isOpen} onOpenChange={handleClostModal}>
-      <DialogContent>
+    <Dialog open={isOpen} onOpenChange={handleCloseModal}>
+      <DialogContent className="max-h-[90vh]">
         <DialogTitle>포스트 작성</DialogTitle>
-        <textarea />
-        <Button>
+        <textarea
+          ref={textareaRef}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="max-h-125 min-h-25 focus:outline-none"
+          placeholder="무슨 일이 있었나요?"
+        />
+        <Button variant="outline" className="cursor-pointer">
           <ImageIcon />
           이미지 추가
         </Button>
-        <Button>저장</Button>
+        <Button className="cursor-pointer">저장</Button>
       </DialogContent>
     </Dialog>,
     modalRoot,
